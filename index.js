24252a0cd57030e00f3dffebb99bfb3930204079
Ishan Verma <ishanv002@gmail.com>
2024-12-22 23:20:18 +0530

    addition of admin router

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
+const { adminRouter } = require("./routes/admin");
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/admin", adminRouter);
